936e862cd2949a54f7ddc627f21b729d3eb79ac1
Auro Soni <aurokumar926@gmail.com>
2024-06-30 15:29:46 +0530

    app complete

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 // Modules
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const windowStateKeeper = require('electron-window-state');
+const readItem = require('./readItem');
+const appMenu = require('./menu');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+ipcMain.on('new-item', (e, itemUrl) => {
+    console.log(itemUrl);
+
+    readItem(itemUrl, item => {
+        e.sender.send('new-item-success', item);
+    });
+})
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
@@ -27,6 +37,8 @@ function createWindow() {
             nodeIntegration: true
         }
     })
+
+    appMenu(mainWindow.webContents)
 
     windowState.manage(mainWindow)
 
